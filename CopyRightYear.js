fed3cc9d23f6cc75f8042e910c7b35bdad2d8ee4
Dylan Royle <dylanroyle@hotmail.co.uk>
2023-05-04 19:35:36 +0100

    Copyright and Birthday are in one javascript file

--- CopyRightYear.js
+++
@@ -1,2 +0,0 @@
-var year = new Date().getFullYear();
-document.write("&copy; " + year + " Dylan Royle. Some rights reserved. Icons are owned by their respective copyright and trademark holders.");
--- Site.js
+++ Site.js
@@ -23,3 +23,19 @@ function openPopup() {
     document.getElementById("twitch-popup").classList.remove("popup-animation");
   }
   
+  var year = new Date().getFullYear();
+  document.write("&copy; " + year + " Dylan Royle. Some rights reserved. Icons are owned by their respective copyright and trademark holders.");
+
+  function calculate() {
+    const birthday = new Date("2003-09-06");
+    const now = new Date();
+    
+    let nextBirthday = new Date(now.getFullYear(), birthday.getMonth(), birthday.getDate());
+    if (nextBirthday < now) {
+      nextBirthday = new Date(now.getFullYear() + 1, birthday.getMonth(), birthday.getDate()); }
+    let yearsRemaining = nextBirthday.getFullYear() - birthday.getFullYear();
+    if (now < nextBirthday) { yearsRemaining--; }
+    
+    document.getElementById("countdown").innerHTML = yearsRemaining;
+    setTimeout(calculate, 1000);} 
+    window.onload = calculate;
