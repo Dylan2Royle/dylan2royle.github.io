a4ae658a4ece142d2ff8393a3e335dc8f5168bcc
Dylan Royle <dylanroyle@hotmail.co.uk>
2023-05-04 15:59:02 +0100

    Update the site.js and Fix The Twitch Links

--- Site.js
+++ Site.js
@@ -8,6 +8,18 @@ function openPopup() {
     document.getElementById("popup").classList.remove("popup-animation");
   }
 
-
-
+  function openTwitchPopup() {
+    document.getElementById("twitch-popup").style.display = "block";
+    document.getElementById("twitch-popup").classList.add("popup-animation");
+  }
+  
+  function closeTwitchPopup() {
+    document.getElementById("twitch-popup").style.display = "none";
+    document.getElementById("twitch-popup").classList.remove("popup-animation");
+  }
+  
+  function resetTwitchPopup() {
+    document.getElementById("twitch-popup").style.display = "none";
+    document.getElementById("twitch-popup").classList.remove("popup-animation");
+  }
   
