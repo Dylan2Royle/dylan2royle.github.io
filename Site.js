7ab2d85defd4a1845063c63c2fb46f79ba127f09
Dylan Royle <dylanroyle@hotmail.co.uk>
2023-05-04 10:31:49 +0100

    Added Twitch and a JS For the Site

---
+++ Site.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("popup").classList.add("popup-animation");
+  }
+
+  function closePopup() {
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("popup").classList.remove("popup-animation");
+  }
