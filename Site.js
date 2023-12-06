6844381d099ed380afd2f466bc811265cb52a8f9
Dylan Royle <enquiries@dylanroyle.co.uk>
2023-12-06 10:34:44 +0000

    New

--- Site.js
+++ Site.js
@@ -1,76 +1,50 @@
-// Pop up and pop down
-function openPopup() {
-    document.getElementById("popup").style.display = "block";
-    document.getElementById("popup").classList.add("popup-animation");
+
+// Birthday CountDown 
+function calculateAge(birthday) {
+  const now = new Date();
+
+  let nextBirthday = new Date(now.getFullYear(), birthday.getMonth(), birthday.getDate());
+  if (nextBirthday < now) {
+    nextBirthday = new Date(now.getFullYear() + 1, birthday.getMonth(), birthday.getDate());
+  }
+  let yearsRemaining = nextBirthday.getFullYear() - birthday.getFullYear();
+  if (now < nextBirthday) {
+    yearsRemaining--;
   }
 
-  function closePopup() {
-    document.getElementById("popup").style.display = "none";
-    document.getElementById("popup").classList.remove("popup-animation");
-  }
+  return yearsRemaining;
+}
 
-  function openTwitchPopup() {
-    document.getElementById("twitch-popup").style.display = "block";
-    document.getElementById("twitch-popup").classList.add("popup-animation");
-  }
-  
-  function closeTwitchPopup() {
-    document.getElementById("twitch-popup").style.display = "none";
-    document.getElementById("twitch-popup").classList.remove("popup-animation");
-  }
-  
-  function resetTwitchPopup() {
-    document.getElementById("twitch-popup").style.display = "none";
-    document.getElementById("twitch-popup").classList.remove("popup-animation");
-  }
+function displayAge() {
+  const birthday = new Date("2003-09-06");
+  const age = calculateAge(birthday);
+  document.getElementById("Countdown").textContent = age;
+}
 
-  
-  // Birthday CountDown 
-  function calculateAge(birthday) {
-    const now = new Date();
-    
-    let nextBirthday = new Date(now.getFullYear(), birthday.getMonth(), birthday.getDate());
-    if (nextBirthday < now) {
-      nextBirthday = new Date(now.getFullYear() + 1, birthday.getMonth(), birthday.getDate());
-    }
-    let yearsRemaining = nextBirthday.getFullYear() - birthday.getFullYear();
-    if (now < nextBirthday) {
-      yearsRemaining--;
-    }
-    
-    return yearsRemaining;
-  }
-  
-  function displayAge() {
-    const birthday = new Date("2003-09-06");
-    const age = calculateAge(birthday);
-    document.getElementById("Countdown").textContent = age;
-  }
-  
-  window.onload = function() {
-    displayAge();
-    startTime();
-  };
-  
+window.onload = function () {
+  displayAge();
+  startTime();
+};
 
-  // Social Media 
-  const dropdown = document.querySelector('.dropdown');
 
-  dropdown.addEventListener('mouseleave', () => {
+// Social Media 
+const dropdown = document.querySelector('.dropdown');
+
+dropdown.addEventListener('mouseleave', () => {
+  const dropdownContent = document.querySelector('.dropdown-content');
+  dropdownContent.style.display = 'none';
+});
+
+document.querySelectorAll('.dropdown-content a').forEach((item) => {
+  item.addEventListener('click', () => {
     const dropdownContent = document.querySelector('.dropdown-content');
     dropdownContent.style.display = 'none';
   });
-  
-  document.querySelectorAll('.dropdown-content a').forEach((item) => {
-    item.addEventListener('click', () => {
-      const dropdownContent = document.querySelector('.dropdown-content');
-      dropdownContent.style.display = 'none';
-    });
-  });
-  
+});
+
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     for (var i = 0; i < dropdowns.length; i++) {
---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Add event listeners for button clicks
+
+    document.getElementById("youtube").addEventListener("click", function () {
+        window.open("https://www.youtube.com/@DylanRVods/live", "_blank");
+    });
+
+    document.getElementById("kick").addEventListener("click", function () {
+        window.open("https://kick.com/DylanRoyleGaming", "_blank");
+    });
+
+    document.getElementById("twitch").addEventListener("click", function () {
+        window.open("https://www.twitch.tv/DylanRoyleGaming", "_blank");
+    });
+
+    document.getElementById("Instagram").addEventListener("click", function () {
+        window.open("https://Instagram.com/Dylan_R_Live", "_blank");
+    });
+
+    document.getElementById("Twitter").addEventListener("click", function () {
+        window.open("https://twitter.com/Dylan_R_Live", "_blank");
+    });
+
+    document.getElementById("Tiktok").addEventListener("click", function () {
+        window.open("https://www.tiktok.com/@dylan_ryt", "_blank");
+    });
+
+    document.getElementById("threads").addEventListener("click", function () {
+        window.open("https://www.threads.net/Dylan_R_Live", "_blank");
+    });
+
+    document.getElementById("HomePage").addEventListener("click", function () {
+        window.open("index.html");
+    });
+
+});
