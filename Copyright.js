3fde7afdb00e1fd1ab0f4df5c56065bb1cd8b3bc
Dylan Royle <enquiries@dylanroyle.co.uk>
2023-12-08 13:33:54 +0000

    Change up some
    Change the button and rize the box :)

--- Copyright.js
+++ Copyright.js
@@ -1,2 +1,3 @@
 var year = new Date().getFullYear();
 document.write("&copy; " + year + " Dylan Royle. Some rights reserved. Icons are owned by their respective copyright and trademark holders.");
+
